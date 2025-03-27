57cbab43c03a95a0d78c4c489e1100548e9f230c
raman martynau <yaretsroman@gmail.com>
2025-03-27 21:48:43 +0300

    added popup

---
+++ src/constants/barbers.js
@@ -0,0 +1,58 @@
+export const barbers = [
+  {
+    preview: './assets/images/barbers/barber1.jpg',
+    title: 'Darrell Steward',
+    description:
+      'Describe the feedback or end result the person received after interacting with you. What exactly did it give him. You can describe the result in numbers to be specific.',
+    link: 'Read full testimonial',
+  },
+  {
+    preview: './assets/images/barbers/barber2.jpg',
+    title: 'Marvin McKinney',
+    description:
+      'Describe the feedback or end result the person received after interacting with you. What exactly did it give him. You can describe the result in numbers to be specific.',
+    link: 'Read full testimonial',
+  },
+  {
+    preview: './assets/images/barbers/barber3.jpg',
+    title: 'Ronald Richards',
+    description:
+      'Describe the feedback or end result the person received after interacting with you. What exactly did it give him. You can describe the result in numbers to be specific.',
+    link: 'Read full testimonial',
+  },
+  {
+    preview: './assets/images/barbers/barber4.jpg',
+    title: 'Cody Fisher',
+    description:
+      'Describe the feedback or end result the person received after interacting with you. What exactly did it give him. You can describe the result in numbers to be specific.',
+    link: 'Read full testimonial',
+  },
+  {
+    preview: './assets/images/barbers/barber1.jpg',
+    title: 'Darrell Steward',
+    description:
+      'Describe the feedback or end result the person received after interacting with you. What exactly did it give him. You can describe the result in numbers to be specific.',
+    link: 'Read full testimonial',
+  },
+  {
+    preview: './assets/images/barbers/barber2.jpg',
+    title: 'Marvin McKinney',
+    description:
+      'Describe the feedback or end result the person received after interacting with you. What exactly did it give him. You can describe the result in numbers to be specific.',
+    link: 'Read full testimonial',
+  },
+  {
+    preview: './assets/images/barbers/barber3.jpg',
+    title: 'Ronald Richards',
+    description:
+      'Describe the feedback or end result the person received after interacting with you. What exactly did it give him. You can describe the result in numbers to be specific.',
+    link: 'Read full testimonial',
+  },
+  {
+    preview: './assets/images/barbers/barber4.jpg',
+    title: 'Cody Fisher',
+    description:
+      'Describe the feedback or end result the person received after interacting with you. What exactly did it give him. You can describe the result in numbers to be specific.',
+    link: 'Read full testimonial',
+  },
+];
--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import './components/button/button';
 import './components/benefits/benefits';
 import './components/about-us/about-us';
 import './components/testimonials/testimonials';
+import './components/card/card.js';
 import './components/barbers-banner/barbers-banner';
 import './components/price/price';
 import './components/consultation/consultation';
@@ -18,6 +19,7 @@ import './components/form/form';
 import './components/contacts/contacts';
 import './components/footer/footer';
 import 'bootstrap/dist/css/bootstrap-grid.css';
+import './components/popup/popup.js';
 import './components/drawer/drawer';
 
 console.log('Hello IT-academy student 🚀');
