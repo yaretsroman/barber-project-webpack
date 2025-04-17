0d44c57f01ea33d8ccfcd2fb1f61d8ed46e4ccae
raman martynau <yaretsroman@gmail.com>
2025-04-17 18:15:57 +0300

    addad to top button

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,1 +1,20 @@
 import './button.scss';
+
+const topBtn = document.querySelector('.to-top-btn');
+
+window.addEventListener('scroll', checkHeight);
+
+function checkHeight() {
+  if (window.scrollY > 250) {
+    topBtn.style.display = 'flex';
+  } else {
+    topBtn.style.display = 'none';
+  }
+}
+
+topBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
