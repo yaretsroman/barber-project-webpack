f21b4533296ff7334aaaee113dd213884dd200f4
raman martynau <yaretsroman@gmail.com>
2025-04-17 13:58:37 +0300

    fixed closing drawer

--- src/components/drawer/drawer.js
+++ src/components/drawer/drawer.js
@@ -6,3 +6,15 @@ const drawer = document.querySelector('.drawer');
 closeBtn.addEventListener('click', () => {
   drawer.classList.remove('drawer--active');
 });
+
+const onClickOudside = (elementId, callback) => {
+  const element = document.getElementById(elementId);
+
+  document.addEventListener('click', (e) => {
+    if (!element.contains(e.target)) callback();
+  });
+};
+
+onClickOudside('drawer-id', () => drawer.classList.remove('drawer--active'));
+
+document.addEventListener('DOMContentLoaded', onClickOudside);
